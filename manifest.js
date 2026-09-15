6d2f7f197e4630282b07e9a37f70f235a8d3bb4f
sciencelabweb <sciencelabweb@gmail.com>
2026-09-15 17:05:56 +0530

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,24 @@
+{
+  "name": "Science Lab Pro",
+  "short_name": "Science Lab Pro",
+  "description": "Science Simulators",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#177D81",
+  "orientation": "any",
+  "icons": [
+    {
+      "src": "./icon192.jpg",
+      "sizes": "192x192",
+      "type": "image/jpeg",
+      "purpose": "any maskable"
+    },
+    {
+      "src": "./icon512.jpg",
+      "sizes": "512x512",
+      "type": "image/jpeg",
+      "purpose": "any maskable"
+    }
+  ]
+}
